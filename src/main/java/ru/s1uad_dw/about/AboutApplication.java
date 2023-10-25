9d29452a4492cac9517cb5f83f8a8784036c5487
s1uad_dw <s1uad.dw@gmail.com>
2023-10-25 15:42:01 +0300

    test

--- src/main/java/ru/s1uad_dw/about/AboutApplication.java
+++ src/main/java/ru/s1uad_dw/about/AboutApplication.java
@@ -2,8 +2,11 @@ package ru.s1uad_dw.about;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import ru.s1uad_dw.about.controllers.TestController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=TestController.class)
 public class AboutApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ru/s1uad_dw/about/controllers/TestController.java
@@ -0,0 +1,13 @@
+package ru.s1uad_dw.about.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String abab(){
+        return "abab";
+    }
+}
