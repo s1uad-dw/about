4b98324526f21f0c6eb8e648e51cf075600e4af8
s1uad_dw <s1uad.dw@gmail.com>
2023-10-25 09:11:19 +0300

    add initial files

---
+++ src/test/java/ru/s1uad_dw/about/AboutApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.s1uad_dw.about;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
